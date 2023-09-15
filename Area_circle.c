a964183b7298f5650df0ebb6fa638cef5e68017c
MeetPatel-161098 <145090442+MeetPatel-161098@users.noreply.github.com>
2023-09-15 09:42:10 +0530

    Add files via upload

---
+++ Area_circle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int r;
+	float area, pi=3.14;
+	
+	printf("\t Enter the value of redious : ");
+	scanf("%d",&r);
+	
+	area=pi*r*r;
+	
+	printf("\t Area of circle is : %.2f ",area);
+	
+}
+
+// sqeare or triangle
